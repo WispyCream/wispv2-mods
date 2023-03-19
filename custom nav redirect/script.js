567292a5f7c63c1bd1c99ad8e93e9cdf9eb7d627
Christian Krenn <77990771+krenny@users.noreply.github.com>
2023-03-19 10:57:00 +0100

    feat: multiple navbar links

--- custom nav redirect/script.js
+++ custom nav redirect/script.js
@@ -1,22 +1,42 @@
 window.addEventListener("load", function () {
-    let e = "https://bad.is-having.fun",
-        t = "Go to Bad's Site";
+  buttons = {
+    "Go to Bad's Site": "https://bad.is-having.fun",
+    Krenny: "http://krenny.is-having.fun",
+    WispyCream: "https://github.com/WispyCream/wispv2-mods/tree/main/custom%20nav%20redirect",
+  };
+
+  Object.keys(buttons).forEach((title) => {
     var s = setInterval(function () {
-        try {
-            let n = document.getElementsByClassName("absolute top-0 left-0 w-full");
-            clearInterval(s);
-            let a = document.createElement("li"),
-                l = document.createElement("li");
-            a.setAttribute("id", "yesBadCustomNav1"),
-                l.setAttribute("id", "yesBadCustomNav2"),
-                a.setAttribute("class", "px-6 py-3"),
-                l.setAttribute("class", "px-6 py-3"),
-                n[0].insertAdjacentElement("beforeend", a),
-                n[1].insertAdjacentElement("beforeend", l),
-                (document.getElementById("yesBadCustomNav1").innerHTML =
-                    "<span class='focus:outline-none' tabindex='0' githubyesbad=''><a href=" + e + " class='cursor-pointer router-link flex justify-between'><p>" + t + "</p><p class='text-white'> • </p></a></span>"),
-                (document.getElementById("yesBadCustomNav2").innerHTML =
-                    "<span class='focus:outline-none' tabindex='0' githubyesbad=''><a href=" + e + " class='cursor-pointer router-link flex justify-between'><p>" + t + "</p><p class='text-white'> • </p></a></span>");
-        } catch (i) {}
+      try {
+        let n = document.getElementsByClassName("absolute top-0 left-0 w-full");
+        clearInterval(s);
+        let a = document.createElement("li"),
+          l = document.createElement("li");
+        a.setAttribute("id", "yesBadCustomNav1" + buttons[title]),
+          l.setAttribute("id", "yesBadCustomNav2" + buttons[title]),
+          a.setAttribute("class", "px-6 py-3"),
+          l.setAttribute("class", "px-6 py-3"),
+          n[0].insertAdjacentElement("beforeend", a),
+          n[1].insertAdjacentElement("beforeend", l),
+          (document.getElementById(
+            "yesBadCustomNav1" + buttons[title]
+          ).innerHTML =
+            "<span class='focus:outline-none' tabindex='0' githubyesbad=''><a href=" +
+            buttons[title] +
+            " class='cursor-pointer router-link flex justify-between'><p>" +
+            title +
+            "</p><p class='text-white'> • </p></a></span>"),
+          (document.getElementById(
+            "yesBadCustomNav2" + buttons[title]
+          ).innerHTML =
+            "<span class='focus:outline-none' tabindex='0' githubyesbad=''><a href=" +
+            buttons[title] +
+            " class='cursor-pointer router-link flex justify-between'><p>" +
+            title +
+            "</p><p class='text-white'> • </p></a></span>");
+      } catch (i) {
+        console.log(i);
+      }
     }, 600);
+  });
 });
